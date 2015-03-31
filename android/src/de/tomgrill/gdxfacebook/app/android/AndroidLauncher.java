29fdfbe2f1de899d0f9301a70563fde8dc909ba0
Thomas Pronold <tp@tpronold.de>
2015-04-01 00:33:45 +0200

    upgrade Android Facebook SDK 4.0.0

--- android/src/de/tomgrill/gdxfacebook/app/android/AndroidLauncher.java
+++ android/src/de/tomgrill/gdxfacebook/app/android/AndroidLauncher.java
@@ -2,12 +2,12 @@ package de.tomgrill.gdxfacebook.app.android;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-import de.tomgrill.gdxfacebook.android.GdxFacebookAndroidApplication;
 import de.tomgrill.gdxfacebook.app.GdxFacebookSampleApp;
 
-public class AndroidLauncher extends GdxFacebookAndroidApplication {
+public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
