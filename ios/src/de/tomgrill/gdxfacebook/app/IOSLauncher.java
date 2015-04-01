229f09c02282eb1c30843b87f4c8388e871f1db0
Thomas Pronold <tpa@tpronold.de>
2015-04-01 12:52:12 +0200

    removed finishedLoading hooking

--- ios/src/de/tomgrill/gdxfacebook/app/IOSLauncher.java
+++ ios/src/de/tomgrill/gdxfacebook/app/IOSLauncher.java
@@ -1,7 +1,11 @@
 package de.tomgrill.gdxfacebook.app;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSPropertyList;
+import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.bindings.facebook.manager.FacebookManager;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
@@ -12,6 +16,10 @@ public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.orientationLandscape = false;
+		config.orientationPortrait = true;
+		config.useCompass = false;
+		config.useAccelerometer = false;
         return new IOSApplication(new GdxFacebookSampleApp(), config);
     }
 
@@ -20,4 +28,24 @@ public class IOSLauncher extends IOSApplication.Delegate {
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
     }
+    
+    @Override
+    public void didBecomeActive (UIApplication application) {
+    	super.didBecomeActive(application);
+	    // You need to add this line, otherwise Facebook will not work correctly!
+	    FacebookManager.getInstance().handleDidBecomeActive(application);
+    }
+    @Override
+    public boolean openURL (UIApplication application, NSURL url, String sourceApplication, NSPropertyList annotation) {
+    	// You need to add this line, otherwise Facebook will not work correctly!
+		return FacebookManager.getInstance().handleOpenURL(application, url, sourceApplication, annotation);		
+    }
+    @Override
+    public void willTerminate (UIApplication application) {
+	    // You need to add this line, otherwise Facebook will not work correctly!
+	    FacebookManager.getInstance().handleWillTerminate(application);	
+		super.willTerminate(application);
+    }
+    
+  
 }
