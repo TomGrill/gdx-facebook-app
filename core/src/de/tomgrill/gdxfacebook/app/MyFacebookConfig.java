15c25d398627390e2bd3f9395a8c63366631f3eb
Thomas Pronold <tp@tpronold.de>
2015-04-01 13:36:10 +0200

    added config files to git index.

---
+++ core/src/de/tomgrill/gdxfacebook/app/MyFacebookConfig.java
@@ -0,0 +1,11 @@
+package de.tomgrill.gdxfacebook.app;
+
+import de.tomgrill.gdxfacebook.core.FacebookConfig;
+
+public class MyFacebookConfig extends FacebookConfig {
+	public MyFacebookConfig() {
+		APP_ID = "1559365201012064";
+
+	}
+
+}
