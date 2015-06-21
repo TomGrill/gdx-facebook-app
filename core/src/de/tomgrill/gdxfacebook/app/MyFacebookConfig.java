0b84ad6f628c01fa23a8e5d5d5ef3f89e635fdab
Thomas Pronold <tp@tpronold.de>
2015-06-21 11:48:13 +0200

    added MyFacebookConfig

---
+++ core/src/de/tomgrill/gdxfacebook/app/MyFacebookConfig.java
@@ -0,0 +1,14 @@
+package de.tomgrill.gdxfacebook.app;
+
+import de.tomgrill.gdxfacebook.core.GDXFacebookConfig;
+
+public class MyFacebookConfig extends GDXFacebookConfig {
+	public MyFacebookConfig() {
+		/**
+		 * TODO Set your App ID
+		 */
+		APP_ID = "REPLACE_WITH_YOUR_APP_ID";
+
+	}
+
+}
