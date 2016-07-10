05bd76441bf794109bfd9db3356fcc86c2eefb71
Thomas Pronold <tp@tpronold.de>
2016-07-10 20:43:55 +0200

    ios-moe setup

--- ios/src/de/tomgrill/gdxfacebook/app/IOSLauncher.java
+++ ios/src/de/tomgrill/gdxfacebook/app/IOSLauncher.java
@@ -5,6 +5,7 @@ import org.robovm.apple.foundation.NSPropertyList;
 import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationLaunchOptions;
+import org.robovm.apple.uikit.UIApplicationOpenURLOptions;
 import org.robovm.pods.facebook.core.FBSDKAppEvents;
 import org.robovm.pods.facebook.core.FBSDKApplicationDelegate;
 
@@ -35,6 +36,7 @@ public class IOSLauncher extends IOSApplication.Delegate {
 		// correctly!
 		FBSDKAppEvents.activateApp();
 	}
+
 
 	@Override
 	public boolean openURL(UIApplication application, NSURL url, String sourceApplication, NSPropertyList annotation) {
