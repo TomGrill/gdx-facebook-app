22b11f194e51396473cf53dfe0aadd5a09636ebc
Thomas Pronold <mail@tomgrill.de>
2017-08-02 20:22:38 +0200

    Add FragmentAcitivity example launcher for testing

--- android/src/de/tomgrill/gdxfacebook/app/android/AndroidLauncher.java
+++ android/src/de/tomgrill/gdxfacebook/app/android/AndroidLauncher.java
@@ -1,17 +1,54 @@
 package de.tomgrill.gdxfacebook.app.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
 import de.tomgrill.gdxfacebook.app.GdxFacebookSampleApp;
 
+/*
+    DEFAULT LIBGDX SETUP SETUP
+ */
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GdxFacebookSampleApp(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new GdxFacebookSampleApp(), config);
+    }
 }
+
+/*
+    FRAGMENT ACTIVITY SETUP
+ */
+//public class AndroidLauncher extends FragmentActivity implements AndroidFragmentApplication.Callbacks {
+//
+//    private GameFragment gameFragment;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        gameFragment = new GameFragment();
+//        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+//        trans.replace(android.R.id.content, gameFragment);
+//        trans.commit();
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        gameFragment.onActivityResult(requestCode, resultCode, data);
+//    }
+//
+//    @Override
+//    public void exit() {
+//
+//    }
+//
+//    public static class GameFragment extends AndroidFragmentApplication {
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//            return initializeForView(new GdxFacebookSampleApp(), new AndroidApplicationConfiguration());
+//        }
+//    }
+//}
